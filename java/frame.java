c63a32905252318d400a311758d34917e3a720be
AMULYA . GANDRAJUPALLI <gandrajupallamulya@gmail.com>
2025-05-06 13:03:41 +0530

    Add files via upload

---
+++ java/frame.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+class Apps
+{
+public static void main(String args[])
+{
+Frame f=new Frame();
+f.setTitle("Notepad");
+f.setSize(200,500);
+f.setVisible(true);
+Label l1=new Label("Reg No");
+f.add(l1);
+Button b1=new Label("Submit");
+f.add(b1);
+}
+}
